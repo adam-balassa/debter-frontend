dc4692117439bc8a233a8d0a69ea34a711dd6bab
Adam <balassaadi@gmail.com>
2019-08-31 15:15:58 +0200

    hotfix

--- src/app/models/request.model.ts
+++ src/app/models/request.model.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Response, Success, Error } from './response.model';
 
-const API = 'http://debter-app.herokuapp.com';
+const API = 'https://debter-app.herokuapp.com';
 
 export class Request<ResultType = any> {
   private url: string;
